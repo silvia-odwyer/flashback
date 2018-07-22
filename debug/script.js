0a108381e445f2d2eab2238e1eec790cac1b8681
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-07-22 21:13:00 +0100

    Combination of filter grid filtering, and user event-driven filtering now works.

--- debug/script.js
+++ debug/script.js
@@ -11,11 +11,14 @@
     document.addEventListener('DOMContentLoaded', init, false);
 
     function init() {
+        var blue_grey_scale = document.getElementById('blue_greyscale');
+        blue_grey_scale.addEventListener("click", function () { blueGreyScale(img) }, false);
+
         // Init default image
         img = document.createElement("img");
 
         img.onload = function () {
-            ctx.drawImage(img, 10, 10, 220, 277);
+            ctx.drawImage(img, 0, 0, 220, 277);
 
         }
 
@@ -102,7 +105,7 @@
         }
 
 
-        const cool_twilight = function () {
+        const cool_twilight_filtergrid = function () {
             var new_ctx = current_canvas.getContext("2d");
 
             console.log("solange called");
@@ -125,7 +128,122 @@
             img.crossOrigin = "Anonymous";
         }
 
-        const group = [solange_func, cool_twilight]
+        const blues = function () {
+            var new_ctx = current_canvas.getContext("2d");
+
+            console.log("solange called");
+            img = new Image();
+            img.addEventListener("load", function () {
+                new_ctx.drawImage(img, 0, 0, 220, 277);
+
+
+                newImgData = new_ctx.getImageData(0, 0, current_canvas.width, current_canvas.height);
+                console.log(newImgData);
+
+                for (var i = 0; i < newImgData.data.length; i += 4) {
+                    newImgData.data[i + 2] = 255 - newImgData.data[i + 2];
+                }
+                new_ctx.putImageData(newImgData, 0, 0);
+                console.log(newImgData)
+                return new_canvas;
+            });
+            img.src = current_img_url;
+            img.crossOrigin = "Anonymous";
+        }
+
+        const darkify_filtergrid = function () {
+            var new_ctx = current_canvas.getContext("2d");
+
+            console.log("solange called");
+            img = new Image();
+            img.addEventListener("load", function () {
+                new_ctx.drawImage(img, 0, 0, 220, 277);
+                imgData = new_ctx.getImageData(0, 0, current_canvas.width, current_canvas.height);
+
+                var BRIGHTNESS_ADJ = 20;
+                for (var i = 0; i < imgData.data.length; i += 4) {
+                    imgData.data[i] -= BRIGHTNESS_ADJ
+                    imgData.data[i + 1] -= BRIGHTNESS_ADJ
+                    imgData.data[i + 2] -= BRIGHTNESS_ADJ
+                }
+                new_ctx.putImageData(imgData, 0, 0);
+
+            });
+            img.src = current_img_url;
+            img.crossOrigin = "Anonymous";
+        }
+
+        const incbrightness_filtergrid = function () {
+            var new_ctx = current_canvas.getContext("2d");
+
+            console.log("solange called");
+            img = new Image();
+            img.addEventListener("load", function () {
+                new_ctx.drawImage(img, 0, 0, 220, 277);
+                imgData = new_ctx.getImageData(0, 0, current_canvas.width, current_canvas.height);
+
+                var BRIGHTNESS_ADJ = 50;
+                for (var i = 0; i < imgData.data.length; i += 4) {
+                    imgData.data[i] += BRIGHTNESS_ADJ
+                    imgData.data[i + 1] += BRIGHTNESS_ADJ
+                    imgData.data[i + 2] += BRIGHTNESS_ADJ
+                }
+                new_ctx.putImageData(imgData, 0, 0);
+
+            });
+            img.src = current_img_url;
+            img.crossOrigin = "Anonymous";
+        }
+
+        const greyscale_filtergrid = function () {
+            var new_ctx = current_canvas.getContext("2d");
+
+            console.log("solange called");
+            img = new Image();
+            img.addEventListener("load", function () {
+                new_ctx.drawImage(img, 0, 0, 220, 277);
+                imgData = new_ctx.getImageData(0, 0, current_canvas.width, current_canvas.height);
+
+                for (i = 0; i < imgData.data.length; i += 4) {
+                    var avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+                    imgData.data[i] = avg
+                    imgData.data[i + 1] = avg
+                    imgData.data[i + 2] = avg
+                }
+        
+                new_ctx.putImageData(imgData, 0, 0);
+
+            });
+            img.src = current_img_url;
+            img.crossOrigin = "Anonymous";
+        }
+
+        const redgreyscale_filtergrid = function () {
+            var new_ctx = current_canvas.getContext("2d");
+
+            console.log("solange called");
+            img = new Image();
+            img.addEventListener("load", function () {
+                new_ctx.drawImage(img, 0, 0, 220, 277);
+                imgData = new_ctx.getImageData(0, 0, current_canvas.width, current_canvas.height);
+
+
+                for (var i = 0; i < imgData.data.length; i += 4) {
+                    var avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+                    imgData.data[i] = avg + 100
+                    imgData.data[i + 1] = avg + 40
+                    imgData.data[i + 2] = avg + 20
+                }
+        
+                new_ctx.putImageData(imgData, 0, 0);
+
+            });
+            img.src = current_img_url;
+            img.crossOrigin = "Anonymous";
+        }
+
+
+        const group = [solange_func, cool_twilight_filtergrid, blues, darkify_filtergrid, incbrightness_filtergrid, greyscale_filtergrid, redgreyscale_filtergrid]
 
         var tile_elem;
 
@@ -135,15 +253,6 @@
             console.log("I is on:", i)
             var new_canvas = group[i]();
         }
-        // var one = group[0]()
-        // var tile_un = tiles[0]
-        // tile_un.appendChild(one);
-
-        //     one = group[1]()
-        //     tile_un = tiles[1]
-        //     tile_un.appendChild(one);
-
-
     }
 
     // function darkify(img) {
@@ -162,19 +271,20 @@
     //}
 
 
-    // function blueGreyScale() {
 
-    //     ctx.drawImage(img, 10, 10, 220, 277);
-    //     var imgData = ctx.getImageData(0, 0, c.width, c.height);
+    function blueGreyScale() {
 
-    //     for (var i = 0; i < imgData.data.length; i += 4) {
-    //         var avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
-    //         imgData.data[i] = avg + 20
-    //         imgData.data[i + 1] = avg + 30
-    //         imgData.data[i + 2] = avg + 60
-    //     }
-    //     ctx.putImageData(imgData, 0, 0);
-    // }
+        ctx.drawImage(img, 0, 0, 220, 277);
+        var imgData = ctx.getImageData(0, 0, c.width, c.height);
+
+        for (var i = 0; i < imgData.data.length; i += 4) {
+            var avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+            imgData.data[i] = avg + 20
+            imgData.data[i + 1] = avg + 30
+            imgData.data[i + 2] = avg + 60
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
 
 
     function getRandomNumber(min, max) {
